1bd536fb4a893570759cc5fde3d44fa32540eef6
IrrelevantElephant <harry.dickinson@gmail.com>
2011-08-20 15:21:15 +0100

    Removed code that would display a sample image on the homepage

--- header.php
+++ header.php
@@ -84,15 +84,15 @@
 				<?php
 					// The header image
 					// Check if this is a post or page, if it has a thumbnail, and if it's a big one
+					// HD 20/08/2011 - Altered logic to prevent an image being shown if header is rendered without an existing thumbnail
 					if ( is_singular() &&
 							has_post_thumbnail( $post->ID ) &&
 							( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( HEADER_IMAGE_WIDTH, HEADER_IMAGE_WIDTH ) ) ) &&
 							$image[1] >= HEADER_IMAGE_WIDTH ) :
 						// Houston, we have a new header image!
 						echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
-					else : ?>
-					<img src="<?php header_image(); ?>" width="<?php echo HEADER_IMAGE_WIDTH; ?>" height="<?php echo HEADER_IMAGE_HEIGHT; ?>" alt="" />
-				<?php endif; // end check for featured image or standard header ?>
+				endif; // end check for featured image or standard header ?>
+					
 			</a>
 			<?php endif; // end check for removed header image ?>
 
